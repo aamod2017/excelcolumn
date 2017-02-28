1380a39cdefe901e170e97b3250eae72f6783c2e
aamod <aamod.tiwari@raweng.com>
2017-02-28 12:11:27 +0530

    excelcolumn project

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# excelcolumn
-Get excel sheet horizontal range from values length
+
+excelcolumn
+=================
+
+Get excel table horizontal range from values
+-----------------------------------------
+for Node.js and Browser.
+-------------------------------------
+
+&copy; Aamod Tiwari, Licensed under the MIT-LICENSE
+
+
+
+Example usage
+-------------
+
+```javascript
+
+var excelcolumn = require('excelcolumn');
+
+var range = excelcolumn(26); // should be "Z"
+
+var range2 = excelcolumn(27); // should be "AA"
+
+```
+
+```bash
+npm install excelcolumn --save
+
+```
---
+++ tests/index.js
@@ -0,0 +1,25 @@
+var excelcolumn = require('../index');
+var expect  = require('chai').expect;
+
+describe('excel-range', function(done) {
+	it('should return function', function(done) {
+    	expect(excelcolumn).to.be.a('function');
+    	done();
+  	});
+  	it('should return range A', function(done) {
+    	expect(excelcolumn(1)).to.equal('A');
+    	done();
+	});
+	it('should return range Z', function(done) {
+    	expect(excelcolumn(26)).to.equal('Z');
+    	done();
+	});
+	it('should return range AA', function(done) {
+    	expect(excelcolumn(27)).to.equal('AA');
+    	done();
+	});
+	it('should return range AAA', function(done) {
+    	expect(excelcolumn(702)).to.equal('AAA');
+    	done();
+	});
+});
